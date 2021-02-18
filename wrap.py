630dc9630bf2a87bf37d07a962065e3303031a08
breakthewall <joan.herisson@univ-evry.fr>
2021-02-18 09:45:03 +0100

    fix(wrap.py): fit to new release

--- wrap.py
+++ wrap.py
@@ -13,15 +13,22 @@ def _cli():
 
     with TemporaryDirectory() as temp_d:
 
-        r_code, result = retropath2(args.sinkfile, args.sourcefile, args.rulesfile,
-                                    temp_d,
-                                    args.knime_exec,
-                                    args.max_steps,
-                                    args.topx,
-                                    args.dmin, args.dmax,
-                                    args.mwmax_source, args.mwmax_cof,
-                                    args.timeout,
-                                    args.forward)
+        r_code, result = retropath2(
+            sinkfile = args.sinkfile,
+            sourcefile = args.sourcefile,
+            rulesfile = args.rulesfile,
+            outdir = temp_d,
+            kexec = None,
+            workflow = None,
+            max_steps = args.max_steps,
+            topx = args.topx,
+            dmin = args.dmin,
+            dmax = args.dmax,
+            mwmax_source = args.mwmax_source,
+            mwmax_cof = args.mwmax_cof,
+            timeout = args.timeout,
+            is_forward = args.forward
+        )
 
         print()
         if r_code > 0:
